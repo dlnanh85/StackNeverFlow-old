8e0e1f7d453f2701114fc9660f8bd2387f1361c4
Anh Dinh <dinhlenhatanh2002yahoo@gmail.com>
2023-03-15 11:41:39 +0700

    Blog list frontend completed, need refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,33 @@ blogTags.forEach(tag => {
 })
 
 // Responsive navbar show/hide by button
-nav = document.que
+nav = document.querySelector('nav')
+navButton = document.querySelector('.ti-menu')
+
+if (window.innerWidth <= 1023) {
+    isNavShown = false
+    toggleNav(isNavShown)
+
+    navButton.addEventListener('click', (event) => {
+        isNavShown = isNavShown? false : true
+        toggleNav(isNavShown)
+        event.stopPropagation()
+    })
+
+    function toggleNav(isNavShown) {
+        if (!isNavShown) 
+            nav.classList.add('hidden')
+        else
+            nav.classList.remove('hidden')
+    }
+
+    // Exit nav by touching anywhere else
+    body = document.querySelector('body')
+
+    body.addEventListener('click', () => {
+        if (isNavShown) {
+            isNavShown = false
+            toggleNav(isNavShown)
+        }
+    })
+}
