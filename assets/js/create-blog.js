f2322363f28c57187d29110215803207ef583874
Anh Dinh <dinhlenhatanh2002yahoo@gmail.com>
2023-04-27 20:06:10 +0700

    logic for create-blog and tag multi-selection

--- assets/js/create-blog.js
+++ assets/js/create-blog.js
@@ -5,7 +5,6 @@ function activeField(formField) {
     formField.classList.add('active');
     formField.querySelector('button').classList.remove('hidden');
     (formField.querySelector('input') || formField.querySelector('textarea')).removeAttribute('disabled');
-    formField.querySelector('.tag-list') && formField.querySelector('.tag-list').classList.remove('hidden');
 }
 
 // Activate the first input field
@@ -48,12 +47,37 @@ let tagsSelected = [];
 
 let selectContainer = document.getElementsByClassName('tag-list')[0];
 let selectedContainer = document.getElementsByClassName('selected-tag')[0];
+let tagInput = document.querySelector('.tag-input input');
+let createTag = selectContainer.querySelector('.tag-item-create');
+
+
+tagInput.onfocus = () => {
+    selectContainer.classList.remove('hidden');
+}
+tagInput.onblur = () => {
+    selectContainer.classList.add('hidden');
+}
+
+let isInput = false;
+tagInput.oninput = () => {
+    isInput = tagInput.value !== '';
+
+    if (isInput) {
+        createTag.classList.remove('hidden');
+        createTag.innerText = `Create "${tagInput.value}"`;
+    }
+    else {
+        createTag.classList.add('hidden');
+    }
+}
 
 renderTagSelect(tagsSelect);
 renderTagSelected(tagsSelected);
 
 function renderTagSelect(tagsSelect) {
-    selectContainer.innerHTML = '';
+    selectContainer.innerHTML = '<div class="tag-item tag-item-create hidden w-100 ps-3 py-2"></div>';
+    createTag = selectContainer.querySelector('.tag-item-create');
+    createTagHandle();
 
     let tagsSelectObjs = tagsSelect.map((tag, index) => {
         let tagObj = document.createElement('div');
@@ -73,6 +97,11 @@ function renderTagSelect(tagsSelect) {
             // ADD TO TagsSelected
             tagsSelected.push(tagObj.innerText);
             renderTagSelected(tagsSelected);
+            // CLEAR input
+            tagInput.value = '';
+        }
+        tagObj.onmousedown = (event) => {
+            event.preventDefault();
         }
     });
 }
@@ -91,7 +120,7 @@ function renderTagSelected(tagsSelected) {
         let tagObj = document.createElement('div');
         tagObj.classList.add('selected-tag-item', 'd-flex', 'align-items-center');
         
-        let child1 = document.createElement('div');
+        let child1 = document.createElement('p');
         child1.classList.add('p-1');
         child1.innerText = tag;
 
@@ -114,5 +143,24 @@ function renderTagSelected(tagsSelected) {
             tagsSelect.push(tagObj.innerText);
             renderTagSelect(tagsSelect);
         }
+        tagObj.onmousedown = (event) => {
+            event.preventDefault();
+        }
     })
+}
+
+
+// HANDLE CREATE NEW TAG
+function createTagHandle() {
+    createTag.onclick = () => {
+        // ADD TO TagsSelected
+        tagsSelected.push(tagInput.value);
+        renderTagSelected(tagsSelected);
+        // CLEAR input
+        tagInput.value = '';
+        createTag.classList.add('hidden');
+    }
+    createTag.onmousedown = (event) => {
+        event.preventDefault();
+    }
 }
